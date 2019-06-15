f48abd6a3a635d2970215450017fa1a4469d992f
manav.sethi <manav.sethi@blocketo.com>
2019-06-15 18:02:22 +0530

    second commit

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/Dev-ManavSethi/blogzzz/controllers"
 	"net/http"
 )
 
@@ -9,21 +10,21 @@ func main() {
 	multiplexer := http.NewServeMux()
 
 	//home
-	multiplexer.HandleFunc("/", home)
+	//multiplexer.HandleFunc("/", home)
 
 	//session related functions
 	multiplexer.HandleFunc("/login", controllers.login)
-	multiplexer.HandleFunc("/signup", signup)
-	multiplexer.HandleFunc("/logout", logout)
+	// multiplexer.HandleFunc("/signup", signup)
+	// multiplexer.HandleFunc("/logout", logout)
 
-	//user related functions
-	multiplexer.HandleFunc("/user", userProfile)
-	multiplexer.HandleFunc("/editUser", editUser)
-	multiplexer.HandleFunc("/deleteUser", deleteUser)
+	// //user related functions
+	// multiplexer.HandleFunc("/user", userProfile)
+	// multiplexer.HandleFunc("/editUser", editUser)
+	// multiplexer.HandleFunc("/deleteUser", deleteUser)
 
-	//blog related fucntions
-	multiplexer.HandleFunc("/createBlog", createBlog)
-	multiplexer.HandleFunc("/editBlog", editBlog)
-	multiplexer.HandleFunc("/deleteBlog", deleteBlog)
+	// //blog related fucntions
+	// multiplexer.HandleFunc("/createBlog", createBlog)
+	// multiplexer.HandleFunc("/editBlog", editBlog)
+	// multiplexer.HandleFunc("/deleteBlog", deleteBlog)
 
 }
