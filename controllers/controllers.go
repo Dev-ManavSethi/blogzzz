bc90e979c9142247ec9dedec0e1e21f55dad2eeb
manav.sethi <manav.sethi@blocketo.com>
2019-06-15 17:46:25 +0530

    first commit

---
+++ controllers/controllers.go
@@ -0,0 +1,10 @@
+package controllers
+
+import "net/http"
+
+type Controller interface {
+}
+
+func login(HTTPresponseWrite http.ResponseWriter, HTTPrequest *http.Request) {
+
+}
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+
+	multiplexer := http.NewServeMux()
+
+	//home
+	multiplexer.HandleFunc("/", home)
+
+	//session related functions
+	multiplexer.HandleFunc("/login", controllers.login)
+	multiplexer.HandleFunc("/signup", signup)
+	multiplexer.HandleFunc("/logout", logout)
+
+	//user related functions
+	multiplexer.HandleFunc("/user", userProfile)
+	multiplexer.HandleFunc("/editUser", editUser)
+	multiplexer.HandleFunc("/deleteUser", deleteUser)
+
+	//blog related fucntions
+	multiplexer.HandleFunc("/createBlog", createBlog)
+	multiplexer.HandleFunc("/editBlog", editBlog)
+	multiplexer.HandleFunc("/deleteBlog", deleteBlog)
+
+}
